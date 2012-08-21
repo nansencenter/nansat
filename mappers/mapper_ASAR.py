65fe2bba40c50b01e23c5af431b4b11a3282ed95
asumak <asuka.yamakawa@nersc.no>
2012-08-21 10:07:14 +0000

    calibrate sigma0

--- mappers/mapper_ASAR.py
+++ mappers/mapper_ASAR.py
@@ -10,6 +10,7 @@
 #-------------------------------------------------------------------------------
 from vrt import VRT, Geolocation
 from envisat import Envisat
+import numpy as np
 
 class Mapper(VRT, Envisat):
     ''' VRT with mapping of WKV for ASAR Level 1
@@ -35,8 +36,19 @@ class Mapper(VRT, Envisat):
         # create empty VRT dataset with geolocation only
         VRT.__init__(self, gdalDataset)
 
-        # Create a dictionary for ASAR band and geolocation
-        metaDict = [{'source': fileName, 'sourceBand': 1, 'wkv': 'surface_backwards_scattering_coefficient_of_radar_wave', 'parameters':{'band_name': 'sigma0', 'polarisation': gdalMetadata['SPH_MDS1_TX_RX_POLAR'], 'pass': gdalMetadata['SPH_PASS'], 'warning': 'fake sigma0, not yet calibrated'}}]
+        # compute calibrated sigma0
+        calibrationConst = gdalDataset.GetMetadataItem("MAIN_PROCESSING_PARAMS_ADS_1_CALIBRATION_FACTORS.1.EXT_CAL_FACT", "records")
+        if calibrationConst is not None:
+            rawCountsArray = np.uint32(gdalDataset.GetRasterBand(1).ReadAsArray())
+            incAngleArray = self.incAngleDataset.dataset.GetRasterBand(1).ReadAsArray()
+            sigma0Array = (rawCountsArray**2 * np.sin(incAngleArray * np.pi / 180.0)) / float(calibrationConst)
+            # Create VRT for sigma0
+            self.sigma0 = VRT(array=sigma0Array)
+            metaDict = [{'source': self.sigma0.fileName, 'sourceBand': 1, 'wkv': 'surface_backwards_scattering_coefficient_of_radar_wave', 'parameters':{'band_name': 'sigma0', 'polarisation': gdalMetadata['SPH_MDS1_TX_RX_POLAR'], 'pass': gdalMetadata['SPH_PASS']}}]
+        else:
+            # Create a dictionary for ASAR band and geolocation
+            metaDict = [{'source': fileName, 'sourceBand': 1, 'wkv': 'surface_backwards_scattering_coefficient_of_radar_wave', 'parameters':{'band_name': 'sigma0', 'polarisation': gdalMetadata['SPH_MDS1_TX_RX_POLAR'], 'pass': gdalMetadata['SPH_PASS'], 'warning': 'fake sigma0, not yet calibrated'}}]
+            print "Could not get calibrated sigma0 because of no calibrationConst"
 
         #add dictionary for IncidenceAngle into metaDict
         for iBand in range(self.incAngleDataset.dataset.RasterCount):
