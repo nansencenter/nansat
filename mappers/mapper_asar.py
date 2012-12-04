b18798071cf0eb671524e1a5f2004f333fa33be9
antonk <anton.korosov@nersc.no>
2012-12-04 14:04:19 +0000

    #89: temporary solution: do no calibration if calib.const cannot be read

--- mappers/mapper_asar.py
+++ mappers/mapper_asar.py
@@ -40,33 +40,40 @@ class Mapper(VRT, Envisat):
         VRT.__init__(self, gdalDataset)
 
         # get calibration constant
-        calibrationConst = float(gdalDataset.GetMetadataItem("MAIN_PROCESSING_PARAMS_ADS_1_CALIBRATION_FACTORS.1.EXT_CAL_FACT", "records"))
+        gotCalibration = True
+        try:
+            calibrationConst = float(gdalDataset.GetMetadataItem("MAIN_PROCESSING_PARAMS_ADS_1_CALIBRATION_FACTORS.1.EXT_CAL_FACT", "records"))
+        except:
+            self.logger.warning('Cannot get calibrationConst')
+            gotCalibration = False
+        
 
         # add dictionary for raw counts
         metaDict = [{'src': {'SourceFilename': fileName, 'SourceBand': 1},
                      'dst': {'name': 'RawCounts_%s' % polarization}}]
 
-        # add dictionary for IncidenceAngle (and other ADS variables)
-        for adsVRT in self.adsVRTs:
-            metaDict.append({'src': {'SourceFilename': adsVRT.fileName,
-                                     'SourceBand': 1},
-                             'dst': {'name':  adsVRT.dataset.GetRasterBand(1).GetMetadataItem('name'),
-                                     'units': adsVRT.dataset.GetRasterBand(1).GetMetadataItem('units')}
-                            })
-
-        # add dictionary for sigma0
-        metaDict.append({'src': [{'SourceFilename': fileName,
-                                  'SourceBand': 1,
-                                  'ScaleRatio': np.sqrt(1.0/calibrationConst)},
-                                 {'SourceFilename': self.adsVRTs[0].fileName,
-                                  'SourceBand': 1}],
-
-                        'dst': { 'wkv':  'surface_backwards_scattering_coefficient_of_radar_wave',
-                                 'PixelFunctionType': 'RawcountsIncidenceToSigma0',
-                                 'polarization': polarization,
-                                 'suffix': polarization,
-                                 'pass': gdalMetadata['SPH_PASS'],
-                                 'dataType': 6}})
+        if gotCalibration:
+            # add dictionary for IncidenceAngle (and other ADS variables)
+            for adsVRT in self.adsVRTs:
+                metaDict.append({'src': {'SourceFilename': adsVRT.fileName,
+                                         'SourceBand': 1},
+                                 'dst': {'name':  adsVRT.dataset.GetRasterBand(1).GetMetadataItem('name'),
+                                         'units': adsVRT.dataset.GetRasterBand(1).GetMetadataItem('units')}
+                                })
+    
+            # add dictionary for sigma0
+            metaDict.append({'src': [{'SourceFilename': fileName,
+                                      'SourceBand': 1,
+                                      'ScaleRatio': np.sqrt(1.0/calibrationConst)},
+                                     {'SourceFilename': self.adsVRTs[0].fileName,
+                                      'SourceBand': 1}],
+    
+                            'dst': { 'wkv':  'surface_backwards_scattering_coefficient_of_radar_wave',
+                                     'PixelFunctionType': 'RawcountsIncidenceToSigma0',
+                                     'polarization': polarization,
+                                     'suffix': polarization,
+                                     'pass': gdalMetadata['SPH_PASS'],
+                                     'dataType': 6}})
 
         # add bands with metadata and corresponding values to the empty VRT
         self._create_bands(metaDict)
