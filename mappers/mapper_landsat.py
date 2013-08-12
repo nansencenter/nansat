38e82fd11ecbd9acdf2a5654d5a2b1b3171e8941
asumak <asuka.yamakawa@nersc.no>
2013-08-12 07:41:50 +0000

    LANDSAT8 has different size bands. If band sizes are different from the 1st band, they are unified by changing DstRect in VRT.

--- mappers/mapper_landsat.py
+++ mappers/mapper_landsat.py
@@ -8,6 +8,9 @@
 from vrt import VRT
 import tarfile
 
+# import standard and additional libraries
+from nansat_tools import *
+
 try:
     from osgeo import gdal
 except ImportError:
@@ -22,22 +25,42 @@ class Mapper(VRT):
         tarFile = tarfile.open(fileName)
 
         tarNames = tarFile.getnames()
-        print tarNames
+        #print tarNames
         metaDict = []
         for tarName in tarNames:
             if ((tarName[0] == 'L' or tarName[0] == 'M') and
                (tarName[-4:] == '.TIF' or tarName[-4:] == '.tif')):
-                print tarName
+                #print tarName
                 bandNo = tarName[-6:-4]
                 metaDict.append({
                     'src': {'SourceFilename': '/vsitar/%s/%s' % (fileName, tarName), 'SourceBand':  1},
                     'dst': {'wkv': 'toa_outgoing_spectral_radiance', 'suffix': bandNo}})
-        print metaDict
-        tmpName = metaDict[0]['src']['SourceFilename']
-        print tmpName
-        gdalDatasetTmp = gdal.Open(tmpName)
+        #print metaDict
+        sizeDiffBands = []
+        for iFile in range(len(metaDict)):
+            tmpName = metaDict[iFile]['src']['SourceFilename']
+            gdalDatasetTmp = gdal.Open(tmpName)
+            if iFile == 0:
+                gdalDatasetTmp0 = gdalDatasetTmp
+                xSize = gdalDatasetTmp.RasterXSize
+                ySize = gdalDatasetTmp.RasterYSize
+            elif xSize != gdalDatasetTmp.RasterXSize or ySize != gdalDatasetTmp.RasterYSize:
+                sizeDiffBands.append(iFile)
+
         # create empty VRT dataset with geolocation only
-        VRT.__init__(self, gdalDatasetTmp, **kwargs)
+        VRT.__init__(self, gdalDatasetTmp0, **kwargs)
 
         # add bands with metadata and corresponding values to the empty VRT
         self._create_bands(metaDict)
+
+        if len(sizeDiffBands) != 0:
+            vrtXML = self.read_xml()
+            node0 = Node.create(vrtXML)
+            for iBand in sizeDiffBands:
+                iNodeDstRect = node0.nodeList('VRTRasterBand')[iBand].node('DstRect')
+                iNodeDstRect.replaceAttribute('xSize',str(xSize))
+                iNodeDstRect.replaceAttribute('ySize',str(ySize))
+
+            self.write_xml(str(node0.rawxml()))
+
+
