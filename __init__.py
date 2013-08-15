a94b9b2398aa1936625942cde5b47f0595f9fb34
akorosov <korosov@gmail.com>
2013-08-15 12:56:02 +0200

    Test update at github

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
 
 import warnings
 
