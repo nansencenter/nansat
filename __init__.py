64585c4b5e0e0bd3cdab93e68061b741ef41677a
akorosov <korosov@gmail.com>
2013-08-15 13:12:44 +0200

    testing: reverse __init__

--- __init__.py
+++ __init__.py
@@ -14,7 +14,6 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# 
 
 import warnings
 
