7b160bf5453e08a7265638272628e1417004ef8b
akorosov <korosov@gmail.com>
2013-10-02 13:46:18 +0200

    #35 convert from binned var into glcol-grid

--- mappers/mapper_globcolour_l3b.py
+++ mappers/mapper_globcolour_l3b.py
@@ -27,17 +27,21 @@ class Mapper(VRT):
         gdalMetadata : gdal metadata
         latlonGrid : numpy 2 layered 2D array with lat/lons of desired grid
         '''
+        # test if input files is GLOBCOLOUR L3B
+        iDir, iFile = os.path.split(fileName)
+        iFileName, iFileExt = os.path.splitext(iFile)
+        print 'idir:', iDir, iFile, iFileName[0:5], iFileExt[0:8]
+        assert iFileName[0:4] == 'L3b_' and iFileExt == '.nc'
+
+        # define shape of GLOBCOLOUR grid
+        GLOBCOLOR_ROWS = 180 * 24
+        GLOBCOLOR_COLS = 360 * 24
+
         # define lon/lat grids for projected var
         if latlonGrid is None:
             #latlonGrid = np.mgrid[90:-90:4320j, -180:180:8640j].astype('float16')
             #latlonGrid = np.mgrid[80:50:900j, -10:30:1200j].astype('float16')
             latlonGrid = np.mgrid[47:39:300j, 25:45:500j].astype('float32')
-
-        # test if files is GLOBCOLOUR L3B
-        iDir, iFile = os.path.split(fileName)
-        iFileName, iFileExt = os.path.splitext(iFile)
-        print 'idir:', iDir, iFile, iFileName[0:5], iFileExt[0:8]
-        assert iFileName[0:4] == 'L3b_' and iFileExt == '.nc'
 
         # get list of similar (same date) files in the directory
         simFilesMask = os.path.join(iDir, iFileName[0:30] + '*')
@@ -49,16 +53,14 @@ class Mapper(VRT):
             print 'simFile', simFile
             f = netcdf_file(simFile)
 
-            # get iBinned, index for converting from binned into raw-gridded
+            # get iBinned, index for converting from binned into GLOBCOLOR-grid
             colBinned = f.variables['col'][:]
             rowBinned = f.variables['row'][:]
-            GLOBCOLOR_ROWS = 180 * 24
-            GLOBCOLOR_COLS = 360 * 24
             iBinned = colBinned.astype('uint32') + (rowBinned.astype('uint32') - 1) * GLOBCOLOR_COLS
             colBinned = None
             rowBinned = None
             
-            # get iRawPro, index for converting from raw-gridded to proj-gridded
+            # get iRawPro, index for converting from GLOBCOLOR-grid to latlonGrid
             yRawPro = np.rint(1 + (GLOBCOLOR_ROWS - 1) * (latlonGrid[0] + 90) / 180)
             lon_step_Mat = 1 / np.cos(np.pi * latlonGrid[0] / 180.) / 24.
             xRawPro = np.rint(1 + (latlonGrid[1] + 180) / lon_step_Mat)
@@ -69,6 +71,14 @@ class Mapper(VRT):
             xRawPro = None
             
             for varName in f.variables:
+                # find variable with _mean, eg CHL1_mean
                 if '_mean' in varName:
-                    varData = f.variables[varName][:]
-                    print varName, varData
+                    # read binned data
+                    varBinned = f.variables[varName][:]
+                    # convert to GLOBCOLOR grid
+                    varRawPro = np.zeros([GLOBCOLOR_ROWS, GLOBCOLOR_COLS], 'float32')
+                    varRawPro.flat[iBinned] = varBinned
+                    print varRawPro.shape, varRawPro
+                    
+
+                    
