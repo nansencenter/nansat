58e4d137a0dc17e857593f073621fdec42d6b5e2
akorosov <korosov@gmail.com>
2014-06-26 09:15:20 +0200

    #74 clean up nansat_tools.py: move PointBrowser to a separate file

--- nansat_tools.py
+++ nansat_tools.py
@@ -235,42 +235,6 @@ class GDALError(Error):
     pass
 
 
-class PointBrowser():
-    '''
-    Click on points and get the X-Y coordinates.
-
-    '''
-    def __init__(self, data, **kwargs):
-        self.fig = plt.figure()
-        self.data = data
-        self.ax = self.fig.add_subplot(111)
-        img = self.ax.imshow(self.data, extent=(0, self.data.shape[1],
-                                                0, self.data.shape[0]),
-                             origin='lower', **kwargs)
-        self.fig.colorbar(img)
-        self.points, = self.ax.plot([], [], '+', ms=12, color='b')
-        self.line, = self.ax.plot([], [])
-        self.coordinates = []
-
-    def onclick(self, event):
-        if event.xdata is not None and event.ydata is not None:
-            self.coordinates.append((event.xdata, event.ydata))
-            tCoordinates = map(tuple, zip(*self.coordinates))
-            self.points.set_data(tCoordinates)
-            self.points.figure.canvas.draw()
-            self.line.set_data(tCoordinates)
-            self.line.figure.canvas.draw()
-
-    def get_points(self):
-        self.fig.canvas.mpl_connect('button_press_event', self.onclick)
-        self.fig.axes[0].set_xlim([0, self.data.shape[1]])
-        self.fig.axes[0].set_ylim([0, self.data.shape[0]])
-        text = "1. Please click on the figure and mark a point or draw a line.\n2. Then close the figure."
-        plt.text(0, int(self.data.shape[0]*1.05), text, fontsize=13,
-                 verticalalignment='top', horizontalalignment='left')
-        plt.gca().invert_yaxis()
-        plt.show()
-
 def initial_bearing(lon1, lat1, lon2, lat2):
         '''Initial bearing when traversing from point1 (lon1, lat1)
         to point2 (lon2, lat2)
