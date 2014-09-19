a8067ce502bb3c14d2e48236bec14727eb246c67
mortenwh <morten.wergeland.hansen@gmail.com>
2014-09-19 17:09:52 +0200

    working on mapper for Sentinel-1 level-1. Still a lot to do but getting there...

--- nansat/pixelfunctions/pixelfunctions.c
+++ nansat/pixelfunctions/pixelfunctions.c
@@ -1046,6 +1046,12 @@ double RawcountsIncidenceToSigma0Function(double *b){
 	return (pow(b[0], 2.0) * sin(b[1] *  pi / 180.0));
 }
 
+double Sentinel1CalibrationFunction(double *b){
+
+    return (pow(b[1],2.0)-b[2]) / b[0];
+
+}
+
 double Sigma0HHToSigma0VVFunction(double *b){
 	double pi = 3.14159265;
     double s0hh, factor;
@@ -1133,6 +1139,19 @@ CPLErr NormReflectanceToRemSensReflectance(void **papoSources, int nSources, voi
     return CE_None;
 }
 
+CPLErr Sentinel1Calibration(void **papoSources, 
+		int nSources, void *pData, int nXSize, int nYSize,
+                GDALDataType eSrcType, GDALDataType eBufType,
+                int nPixelSpace, int nLineSpace){
+
+    GenericPixelFunction(Sentinel1CalibrationFunction,
+        papoSources, nSources, pData,
+        nXSize, nYSize, eSrcType, eBufType,
+        nPixelSpace, nLineSpace);
+    
+    return CE_None;
+}
+
 CPLErr RawcountsIncidenceToSigma0(void **papoSources, 
 		int nSources, void *pData, int nXSize, int nYSize,
         GDALDataType eSrcType, GDALDataType eBufType,
@@ -1211,27 +1230,30 @@ void GenericPixelFunction(double f(double*), void **papoSources,
         GDALDataType eSrcType, GDALDataType eBufType,
         int nPixelSpace, int nLineSpace)
 {
-	int ii, iLine, iCol, iSrc;
-	double *bVal, result;
+    int ii, iLine, iCol, iSrc;
+    double *bVal, result;
     bVal = malloc(nSources * sizeof (double));
     
-	/* ---- Set pixels ---- */
-	for( iLine = 0; iLine < nYSize; iLine++ )
-	{
-		for( iCol = 0; iCol < nXSize; iCol++ )
-		{
-			ii = iLine * nXSize + iCol;
-			/* Source raster pixels may be obtained with SRCVAL macro */
-            for (iSrc = 0; iSrc < nSources; iSrc ++)
+    /* ---- Set pixels ---- */
+    for( iLine = 0; iLine < nYSize; iLine++ ){
+        for( iCol = 0; iCol < nXSize; iCol++ ){
+	    ii = iLine * nXSize + iCol;
+	    /* Source raster pixels may be obtained with SRCVAL macro */
+            for (iSrc = 0; iSrc < nSources; iSrc ++){
                 bVal[iSrc] = SRCVAL(papoSources[iSrc], eSrcType, ii);
+                if (iLine==0 && iCol==0){
+                    printf("%d ",iSrc);
+                    printf("%.4f\n",bVal[iSrc]);
+                }
+            }
 
-			result = f(bVal);
+	    result = f(bVal);
 
-			GDALCopyWords(&result, GDT_Float64, 0,
-			              ((GByte *)pData) + nLineSpace * iLine + iCol * nPixelSpace,
-			              eBufType, nPixelSpace, 1);
-		}
+	    GDALCopyWords(&result, GDT_Float64, 0,
+			    ((GByte *)pData) + nLineSpace * iLine + iCol * nPixelSpace,
+			    eBufType, nPixelSpace, 1);
 	}
+    }
 }
 
 // From the 1st to (N-1)th bands are full size (XSize x YSize), 
@@ -1399,6 +1421,8 @@ CPLErr CPL_STDCALL GDALRegisterDefaultPixelFunc()
     GDALAddDerivedBandPixelFunc("Sigma0NormalizedIce", Sigma0NormalizedIce);
     GDALAddDerivedBandPixelFunc("Sigma0HHNormalizedWater", Sigma0HHNormalizedWater);
     GDALAddDerivedBandPixelFunc("Sigma0VVNormalizedWater", Sigma0VVNormalizedWater);
+    GDALAddDerivedBandPixelFunc("Sentinel1Calibration", Sentinel1Calibration);
 
     return CE_None;
 }
+
