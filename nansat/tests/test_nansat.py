7ba5a6a7b06658eaf127e91758054e9a06267035
akorosov <korosov@gmail.com>
2014-09-29 11:06:01 +0200

    #79 test only nansat.tests

--- nansat/tests/test_nansat.py
+++ nansat/tests/test_nansat.py
@@ -31,44 +31,29 @@ class NansatTest(unittest.TestCase):
     def test_resize_by_pixelsize(self):
         n = Nansat(self.test_data)
         n.resize(pixelsize=500, eResampleAlg=1)
-        if sys.version_info < (2, 7):
-            type(n[1]) == np.ndarray
-        else:
-            self.assertIsInstance(n[1], np.ndarray)
+        self.assertEqual(type(n[1]), np.ndarray)
 
     def test_resize_by_factor(self):
         n = Nansat(self.test_data)
         n.resize(0.5, eResampleAlg=1)
-        if sys.version_info < (2, 7):
-            type(n[1]) == np.ndarray
-        else:
-            self.assertIsInstance(n[1], np.ndarray)
+        self.assertEqual(type(n[1]), np.ndarray)
 
     def test_resize_by_width(self):
         n = Nansat(self.test_data)
         n.resize(width=100, eResampleAlg=1)
-        if sys.version_info < (2, 7):
-            type(n[1]) == np.ndarray
-        else:
-            self.assertIsInstance(n[1], np.ndarray)
+        self.assertEqual(type(n[1]), np.ndarray)
 
     def test_resize_by_height(self):
         n = Nansat(self.test_data)
         n.resize(height=500, eResampleAlg=1)
-        if sys.version_info < (2, 7):
-            type(n[1]) == np.ndarray
-        else:
-            self.assertIsInstance(n[1], np.ndarray)
+        self.assertEqual(type(n[1]), np.ndarray)
 
     def test_reproject(self):
         n = Nansat(self.test_data)
         d = Domain("+proj=latlong +datum=WGS84 +ellps=WGS84 +no_defs",
                    "-lle 15 -35 35 -25 -ts 500 500")
         n.reproject(d)
-        if sys.version_info < (2, 7):
-            type(n[1]) == np.ndarray
-        else:
-            self.assertIsInstance(n[1], np.ndarray)
+        self.assertEqual(type(n[1]), np.ndarray)
 
     def tearDown(self):
         # if any test plots are created, they could be deleted here
