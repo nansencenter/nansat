c168507081da9276d39466b5c361d06729e5527e
akorosov <korosov@gmail.com>
2014-11-10 13:32:02 +0100

    add radarsat2 data to test_mappers; nb: rs2 data is not actually available on FTP

--- mapper_tests/mapper_test_archive.py
+++ mapper_tests/mapper_test_archive.py
@@ -43,8 +43,8 @@ class DataForTestingMappers(object):
                 'aster_l1a')
 
         self.download_test_file(
-                'ftp://ftp.nersc.no/pub/python_test_data/cosmoskymed/CSKS4_SCS_B_PP_11_CO_LA_FF_20111215040251_20111215040257.h5',
-                'cosmoskymed')
+                'ftp://ftp.nersc.no/pub/python_test_data/csks/CSKS4_SCS_B_PP_11_CO_LA_FF_20111215040251_20111215040257.h5',
+                'csks')
 
         self.download_test_file(
                 'ftp://ftp.nersc.no/pub/python_test_data/hirlam/DNMI-NEurope.grb',
@@ -71,7 +71,15 @@ class DataForTestingMappers(object):
                 'ncep')
 
         self.download_test_file(
-                'ftp://ftp.nersc.no/pub/python_test_data/radarsat2/RS2_20140716_061819_0076_SCWA_HHHV_SGF_336560_2501_9900957.zip',
+                'ftp://ftp.nersc.no/pub/python_test_data/radarsat2/RS2_20111109_060616_0045_SCNA_HHHV_SGF_164373_9871_6913894',
+                'radarsat2')
+
+        self.download_test_file(
+                'ftp://ftp.nersc.no/pub/python_test_data/radarsat2/RS2_OK57403_PK545457_DK477422_SCWA_20141025_135245_HH_SGF',
+                'radarsat2')
+
+        self.download_test_file(
+                'ftp://ftp.nersc.no/pub/python_test_data/radarsat2/RS2_OK57403_PK539140_DK477416_SCWA_20141022_152035_HH_SGF.ZIP',
                 'radarsat2')
 
         self.download_test_file(
@@ -119,7 +127,7 @@ class DataForTestingMappers(object):
             os.system('curl -so ' + mapperFName + ' ' + inputURL )
             print time.time() - t0
 
-        if not os.path.isfile(mapperFName):
+        if not os.path.exists(mapperFName):
             warnings.warn( """
                     Could not access %s on ftp-site with test data - contact
                     morten.stette@nersc.no to get the ftp-server at NERSC restarted"""
