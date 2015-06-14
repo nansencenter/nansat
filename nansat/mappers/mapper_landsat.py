16cf79b2ad0e5fa6d48d98c2b112faf22f39a469
akorosov <korosov@gmail.com>
2015-06-14 13:05:43 +0200

    #128 add ability to read from bunch of TIF files

--- nansat/mappers/mapper_landsat.py
+++ nansat/mappers/mapper_landsat.py
@@ -5,6 +5,7 @@
 #               under the terms of GNU General Public License, v.3
 #               http://www.gnu.org/licenses/gpl-3.0.html
 import os
+import glob
 import tarfile
 import warnings
 
@@ -13,34 +14,59 @@ from nansat.tools import gdal, np
 from nansat.vrt import VRT
 
 class Mapper(VRT):
-    ''' Mapper for LANDSAT5,6,7.tar.gz files'''
+    ''' Mapper for LANDSAT5,6,7,8 .tar.gz or tif files'''
 
     def __init__(self, fileName, gdalDataset, gdalMetadata,
                        resolution='low', **kwargs):
-        ''' Create LANDSAT VRT from tar.gz files'''
-        # try to open .tar or .tar.gz or .tgz file with tar
-        try:
-            tarFile = tarfile.open(fileName)
-        except:
-            raise WrongMapperError
-
-        # collect names of bands and corresponding sizes
-        # into bandsInfo dict and bandSizes list
-        tarNames = sorted(tarFile.getnames())
+        ''' Create LANDSAT VRT from multiple tif files or single tar.gz file'''
         bandFileNames = []
         bandSizes = []
         bandDatasets = []
-        for tarName in tarNames:
-            # check if TIF files inside TAR qualify
-            if   (tarName[0] in ['L', 'M'] and
-                  os.path.splitext(tarName)[1] in ['.TIF', '.tif']):
-                # open TIF file from TAR using VSI
-                sourceFilename = '/vsitar/%s/%s' % (fileName, tarName)
+
+        if   (fileName.endswith('.tar') or
+              fileName.endswith('.tar.gz') or
+              fileName.endswith('.tgz')):
+            # try to open .tar or .tar.gz or .tgz file with tar
+            try:
+                tarFile = tarfile.open(fileName)
+            except:
+                raise WrongMapperError
+
+            # collect names of bands and corresponding sizes
+            # into bandsInfo dict and bandSizes list
+            tarNames = sorted(tarFile.getnames())
+            for tarName in tarNames:
+                # check if TIF files inside TAR qualify
+                if   (tarName[0] in ['L', 'M'] and
+                      os.path.splitext(tarName)[1] in ['.TIF', '.tif']):
+                    # open TIF file from TAR using VSI
+                    sourceFilename = '/vsitar/%s/%s' % (fileName, tarName)
+                    gdalDatasetTmp = gdal.Open(sourceFilename)
+                    # keep name, GDALDataset and size
+                    bandFileNames.append(sourceFilename)
+                    bandSizes.append(gdalDatasetTmp.RasterXSize)
+                    bandDatasets.append(gdalDatasetTmp)
+        elif ((fileName.startswith('L') or fileName.startswith('M')) and
+              (fileName.endswith('.tif') or
+               fileName.endswith('.TIF') or
+               fileName.endswith('._MTL.txt'))):
+
+            # try to find TIF/tif files with the same name as input file
+            path, coreName = os.path.split(fileName)
+            coreName = os.path.splitext(coreName)[0].split('_')[0]
+            coreNameMask = coreName+'*[tT][iI][fF]'
+            tifNames = sorted(glob.glob(os.path.join(path, coreNameMask)))
+            print path, coreName, coreNameMask, tifNames
+            for tifName in tifNames:
+                sourceFilename = tifName
                 gdalDatasetTmp = gdal.Open(sourceFilename)
                 # keep name, GDALDataset and size
                 bandFileNames.append(sourceFilename)
                 bandSizes.append(gdalDatasetTmp.RasterXSize)
                 bandDatasets.append(gdalDatasetTmp)
+
+        else:
+            raise WrongMapperError
 
         # if not TIF files found - not appropriate mapper
         if not bandFileNames:
