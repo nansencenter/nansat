568a6e3b7cb774a14cee9241786cec9a7817b334
mortenwh <morten.wergeland.hansen@gmail.com>
2017-03-08 16:59:45 +0100

    not yet done with netcdf-cf mapper..

--- nansat/mappers/mapper_netcdfcf.py
+++ nansat/mappers/mapper_netcdfcf.py
@@ -1,12 +1,70 @@
-from nansat.vrt import VRT
+''' Nansat NetCDF-CF mapper
+
+    Check CF-compliance of your files here:
+    http://cfconventions.org/compliance-checker.html
+'''
+import gdal
+
+from nansat.vrt import VRT, GeolocationArray
 from nansat.tools import WrongMapperError
 
 class Mapper(VRT):
 
-    def __init__(filename, gdalDataset, gdalMetadata, *args, **kwargs):
+    def __init__(self, filename, gdal_dataset, gdal_metadata, *args, **kwargs):
 
+        gdal_metadata = self.replace_nc_global(gdal_metadata)
+
+        # Create empty VRT dataset with geo-reference
+        self._create_empty(gdal_dataset, gdal_metadata)
+    
+        import ipdb
+        ipdb.set_trace()
+
+        # Add bands with metadata and corresponding values to the empty VRT
+        self._create_bands(self.band_list(*args, **kwargs))
+        #xsize, ysize = self.ds_size(sub0)
+
+
+        print 'hei'
+
+    def _create_empty(self, gdal_dataset, metadata):
+        subfiles = self.sub_filenames(gdal_dataset)
+        sub0 = gdal.Open(subfiles[0])
+
+        super(Mapper, self).__init__(self, sub0, srcMetadata=metadata)
+
+        if not self.get_projection():
+            # Set geolocation array from bands
+            self.add_geolocationArray(
+                    GeolocationArray(
+                        [s for s in subfiles if 'longitude' in s or
+                            'GEOLOCATION_X_DATASET' in s][0],
+                        [s for s in subfiles if 'latitude' in s or
+                            'GEOLOCATION_Y_DATASET' in s][0]))
+            # Get band projections
+            projections = [gdal.Open(sub).GetProjection() for sub in subfiles if
+                    gdal.Open(sub).GetProjection()]
+            # Check that projection is the same for all bands
+            assert all(proj==projections[0] for proj in projections)
+            # Set projection
+            self.dataset.SetProjection(projections[0])
+
+    def band_list(self):
+        return 0
+
+    def replace_nc_global(self, gdalMetadata):
         if not gdalMetadata:
             raise WrongMapperError
 
-    def add_band_at_time(self, band, time):
-        pass
+        for key in gdalMetadata.keys():
+             gdalMetadata[key.replace('NC_GLOBAL#', '')] = gdalMetadata.pop(
+                                                                key)
+        return gdalMetadata
+
+    def sub_filenames(self, gdal_dataset):
+        # Get filenames of subdatasets
+        sub_datasets = gdal_dataset.GetSubDatasets()
+        return [f[0] for f in sub_datasets]
+
+    #def ds_size(self, ds):
+    #    return ds.RasterXSize, ds.RasterYSize
--- nansat/tests/test_mappers.py
+++ nansat/tests/test_mappers.py
@@ -13,8 +13,8 @@ class MapperTests(unittest.TestCase):
         self.test_file_arome = '/vagrant/shared/test_data/generic/arome_metcoop_default2_5km_20170227T00Z.nc'
         self.tmpfilename = os.path.join(ntd.tmp_data_path, 'test.nc')
 
-    def test_open_netcdf_cf(self):
-        n = Nansat(self.test_file_arctic, mapperName='netcdfCF')
+    #def test_open_netcdf_cf(self):
+    #    n = Nansat(self.test_file_arctic, mapperName='netcdfcf')
 
     def test_open_arome_netcdf_cf_file(self):
         n = Nansat(self.test_file_arome, mapperName='netcdfcf')
