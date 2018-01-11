ff2cd97affdd6e46ad7ff71ee6aba9e125558d92
mortenwh <morten.wergeland.hansen@gmail.com>
2018-01-11 14:25:59 +0100

    Added mock packages and modules to docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+# -- Mock imported packages and modules
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):  
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['gdal', 'numpy', 'scipy', 'scipy.linalg', 'scipy.signal']  
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
