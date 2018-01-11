b079a7503f70b01b4ff9c303a6c937ea6c707159
mortenwh <morten.wergeland.hansen@gmail.com>
2018-01-11 15:17:20 +0100

    changed docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,8 @@ html_context = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
