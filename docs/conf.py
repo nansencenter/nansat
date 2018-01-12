3b2286fd98de593a008c620cff18c031c72162d8
mortenwh <morten.wergeland.hansen@gmail.com>
2018-01-12 16:58:20 +0100

    include readme in index.rst - it doesn't work properly and the readme must be changed.

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
+import os, sys
+from recommonmark.parser import CommonMarkParser
+
 sys.path.insert(0, os.path.abspath('..'))
 
 ## -- Mock imported packages and modules
@@ -58,8 +59,11 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_parsers = {
+        '.md': CommonMarkParser,
+    }
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
