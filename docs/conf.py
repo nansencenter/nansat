b98aa29376dff2875713db31a817c1666d7a6031
mortenwh <morten.wergeland.hansen@gmail.com>
2018-01-12 10:10:27 +0100

    Added some packages to the mock object list

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['gdal', 'numpy', 'scipy', 'scipy.linalg', 'scipy.signal']  
+MOCK_MODULES = ['gdal', 'osr', 'ogr', 'numpy', 'scipy', 'Pillow', 'pythesint', 'cfunits']  
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
