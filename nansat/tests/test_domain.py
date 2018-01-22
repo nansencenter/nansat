ea845c8a4b16208e4ba454626efc4e2c36dc15b8
jeong1park <orepaku.android@gmail.com>
2018-01-22 15:39:24 +0100

    #91 NA-50 clean up

--- nansat/tests/test_domain.py
+++ nansat/tests/test_domain.py
@@ -149,50 +149,6 @@ class DomainTest(unittest.TestCase):
         self.assertEqual(type(test_pass(self)), Domain)
         self.assertEqual(test_pass(self).shape(), self.LAT.shape)
 
-    def test_init_from_strings(self):
-        d = Domain("+proj=latlong +datum=WGS84 +ellps=WGS84 +no_defs",
-                   "-te 25 70 35 72 -ts 2000 2000")
-
-        self.assertEqual(type(d), Domain)
-
-    def test_init_from_epsg_and_te_string(self):
-        d = Domain(4326, "-te 25 70 35 72 -ts 500 500")
-
-        self.assertEqual(type(d), Domain)
-
-    def test_init_from_epsg_and_lle_string(self):
-        d = Domain(4326, "-lle 25 70 35 72 -ts 500 500")
-
-        self.assertEqual(type(d), Domain)
-
-    def test_init_from_lonlat(self):
-        lat, lon = np.mgrid[-90:90:0.5, -180:180:0.5]
-        d = Domain(lon=lon, lat=lat)
-
-        self.assertEqual(type(d), Domain)
-        self.assertEqual(d.shape(), lat.shape)
-
-    def test_init_from_GDALDataset(self):
-        ds = gdal.Open(self.test_file)
-        d = Domain(ds=ds)
-
-        self.assertEqual(type(d), Domain)
-
-    def test_dont_init_from_invalid(self):
-        self.assertRaises(OptionError, Domain)
-        self.assertRaises(OptionError, Domain, None)
-        with self.assertRaises(OptionError):
-            Domain(ds=gdal.Open(self.test_file),
-                   srs="+proj=latlong +datum=WGS84 +ellps=WGS84 +no_defs",
-                   ext="-te 25 70 35 72 -ts 2000 2000")
-        with self.assertRaises(ProjectionError):
-            Domain(ds=gdal.Open(self.test_file),
-                   srs="unmatched srs")
-
-    def test_init_use_AutoCreateWarpedVRT_to_determine_bounds(self):
-        d = Domain(ds=gdal.Open(self.test_file),
-                   srs="+proj=latlong +datum=WGS84 +ellps=WGS84 +no_defs")
-        self.assertEqual(type(d), Domain)
 
     def test_write_kml(self):
         d = Domain(4326, "-te 25 70 35 72 -ts 500 500")
