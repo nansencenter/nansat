6a75c54eb6dc4e8e78f6073ab446e134aabcdc42
mortenwh <morten.wergeland.hansen@gmail.com>
2018-01-22 13:11:48 +0100

    #116: added nbsphinx to build docs from notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,9 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon'] #'numpydoc']
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax,
+    'nbsphinx] 
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -92,7 +94,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
