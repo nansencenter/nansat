91121071c34663c5d0d8026cb777ecae9b7be24f
jeong1park <orepaku.android@gmail.com>
2018-01-22 15:25:25 +0100

    #91 NA-50 test_init_from_srs_and_ext_lle

--- nansat/tests/test_domain.py
+++ nansat/tests/test_domain.py
@@ -42,6 +42,9 @@ class DomainTest(unittest.TestCase):
         self.test_file_projected = os.path.join(ntd.test_data_path, 'stere.tif')
         self.EXTENT_TE_TS = "-te 25 70 35 72 -ts 500 500"
         self.EXTENT_DICT_TE_TS = {'te': [25.0, 70.0, 35.0, 72.0], 'ts': [500.0, 500.0]}
+        self.EXTENT_LLE_TS = "-lle 25 70 35 72 -ts 500 500"
+        self.EXTENT_DICT_LLE_TS = {'lle': [25.0, 70.0, 35.0, 72.0],
+            'te': [25.0, 70.0, 35.0, 72.0], 'ts': [500.0, 500.0]}
         self.GEO_TRANSFORM = [25.0, 0.02, 0.0, 72.0, 0.0, -0.004]
         self.RASTER_X_SIZE = 500
         self.RASTER_Y_SIZE = 500
@@ -119,7 +122,20 @@ class DomainTest(unittest.TestCase):
             mock_NSR().return_value = self.NSR_SRS_EPSG
             type(mock_NSR()).wkt = PropertyMock(return_value=self.NSR_SRS_EPSG_WKT)
             mock_VRT.from_dataset_params.return_value = self.VRT_FROM_DATASET_PARAMS
-            return Domain(ds=self.GDAL_DATASET, srs=self.SRS_PROJ4)
+            return Domain(ds=self.GDAL_DATASET, srs=self.SRS_EPSG)
+        self.assertEqual(type(test_pass(self)), Domain)
+
+    def test_init_from_srs_and_ext_lle(self):
+        @patch('nansat.domain.NSR')
+        @patch('nansat.domain.VRT')
+        @patch.object(Domain, '_create_extent_dict', return_value=self.EXTENT_DICT_TE_TS)
+        @patch.object(Domain, '_convert_extentDic', return_value=self.EXTENT_DICT_LLE_TS)
+        @patch.object(Domain, '_get_geotransform', return_value=(self.GEO_TRANSFORM, self.RASTER_X_SIZE, self.RASTER_Y_SIZE))
+        def test_pass(self, mock__get_geotransform, mock__convert_extentDic, mock__create_extent_dict, mock_VRT, mock_NSR):
+            mock_NSR().return_value = self.NSR_SRS_EPSG
+            type(mock_NSR()).wkt = PropertyMock(return_value=self.NSR_SRS_EPSG_WKT)
+            mock_VRT.from_dataset_params.return_value = self.VRT_FROM_DATASET_PARAMS
+            return Domain(ds=self.GDAL_DATASET, srs=self.SRS_EPSG)
         self.assertEqual(type(test_pass(self)), Domain)
 
     def test_init_from_strings(self):
