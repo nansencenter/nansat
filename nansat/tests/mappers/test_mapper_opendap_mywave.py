598404b1ec520e03a82c5c8b6aaa4bc56726358d
Artem Moiseev <korvinos@bk.ru>
2018-10-23 08:09:04 +0200

    #382 remove nc dataset from test

--- nansat/tests/mappers/test_mapper_opendap_mywave.py
+++ nansat/tests/mappers/test_mapper_opendap_mywave.py
@@ -1,6 +1,5 @@
 import unittest
 from nansat.mappers.mapper_opendap_mywave import Mapper
-from netCDF4 import Dataset
 
 
 class MyWaveOpenDAPTests(unittest.TestCase):
@@ -8,7 +7,6 @@ class MyWaveOpenDAPTests(unittest.TestCase):
     def setUp(self):
         self.src = 'http://thredds.met.no/thredds/dodsC/fou-hi/mywavewam4archive' \
                    '/2017/10/29/MyWave_wam4_WAVE_20171029T18Z.nc'
-        self.ds = Dataset(self.src)
 
     def test_get_date(self):
         res = Mapper.get_date(self.src)
