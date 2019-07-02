e952a891893fe5ffb5b215270c8a61dffd302c91
mortenwh <morten.wergeland.hansen@gmail.com>
2019-07-02 17:37:20 +0200

    #412: added netcdf-cf mapper for sentinel-1, and a module (sentinel1.py) for generic handling of both netcdf and opendap datasets (see #391). Did not write tests but tried the code on the dataset linked in the issue description. Nansat can now read s1 netcdf files from the Norwegian ground segment but reading some of the bands is *very* slow. This is most likely an issue with the way the files are created. Have e-mailed met.no about it.

---
+++ nansat/mappers/sentinel1.py
@@ -0,0 +1,149 @@
+import json
+import numpy as np
+try:
+    import scipy
+except:
+    IMPORT_SCIPY = False
+else:
+    IMPORT_SCIPY = True
+
+import gdal
+from netCDF4 import Dataset
+
+import pythesint as pti
+
+from nansat.nsr import NSR
+from nansat.vrt import VRT
+from nansat.tools import initial_bearing
+
+class Sentinel1(VRT):
+    """ Mapper class to access Sentinel-1 data with netCDF4 to work for both opendap streams and
+    local files.
+    """
+    timeVarName = 'time'
+
+    def __init__(self, filename):
+        if not IMPORT_SCIPY:
+            raise NansatReadError('Sentinel-1 data cannot be read because scipy is not installed')
+
+        self.ds = Dataset(filename)
+        self._remove_geotransform()
+        self._remove_geolocation()
+        self.dataset.SetProjection('')
+        self.dataset.SetGCPs(self.get_gcps(), NSR().wkt)
+        self.add_incidence_angle_band()
+        self.add_look_direction_band()
+        self.set_gcmd_dif_keywords()
+
+    def set_gcmd_dif_keywords(self):
+        mditem = 'entry_title'
+        if not self.dataset.GetMetadataItem(mditem):
+            self.dataset.SetMetadataItem(mditem, self.input_filename)
+        mditem = 'data_center'
+        if not self.dataset.GetMetadataItem(mditem):
+            self.dataset.SetMetadataItem(mditem, json.dumps(pti.get_gcmd_provider('NO/MET')))
+        mditem = 'ISO_topic_category'
+        if not self.dataset.GetMetadataItem(mditem):
+            self.dataset.SetMetadataItem(mditem,
+                pti.get_iso19115_topic_category('Imagery/Base Maps/Earth Cover')['iso_topic_category'])
+
+        mm = pti.get_gcmd_instrument('sar')
+        if self.ds.MISSION_ID=='S1A':
+            ee = pti.get_gcmd_platform('sentinel-1a')
+        else:
+            ee = pti.get_gcmd_platform('sentinel-1b')
+        self.dataset.SetMetadataItem('instrument', json.dumps(mm))
+        self.dataset.SetMetadataItem('platform', json.dumps(ee))
+
+        self.dataset.SetMetadataItem('time_coverage_start',
+                self.dataset.GetMetadataItem('ACQUISITION_START_TIME'))
+        self.dataset.SetMetadataItem('time_coverage_end', 
+        self.dataset.GetMetadataItem('ACQUISITION_STOP_TIME'))
+
+    def get_gcps(self):
+
+        lon = self.ds.variables['GCP_longitude_'+self.ds.polarisation[:2]]
+        lat = self.ds.variables['GCP_latitude_'+self.ds.polarisation[:2]]
+        line = self.ds.variables['GCP_line_'+self.ds.polarisation[:2]]
+        pixel = self.ds.variables['GCP_pixel_'+self.ds.polarisation[:2]]
+
+        gcps = []
+        for i0 in range(0, self.ds.dimensions['gcp_index'].size):
+            gcp = gdal.GCP(float(lon[i0].data), float(lat[i0].data), 0, float(pixel[i0].data),
+                    float(line[i0].data))
+            gcps.append(gcp)
+
+        return gcps
+
+    def add_incidence_angle_band(self):
+        # Get GCP variables
+        pixel = self.ds['GCP_pixel_'+self.ds.polarisation[:2]][:].data
+        line = self.ds['GCP_line_'+self.ds.polarisation[:2]][:].data
+        inci = self.ds['GCP_incidenceAngle_'+self.ds.polarisation[:2]][:].data
+        inci = inci.reshape(np.unique(line[:].data).shape[0],
+                np.unique(pixel[:].data).shape[0])
+
+        # Add incidence angle band
+        inciVRT = VRT.from_array(inci)
+        inciVRT = inciVRT.get_resized_vrt(self.dataset.RasterXSize, self.dataset.RasterYSize, 1)
+        self.band_vrts['inciVRT'] = inciVRT
+        src = {'SourceFilename': self.band_vrts['inciVRT'].filename,
+               'SourceBand': 1}
+        dst = {'wkv': 'angle_of_incidence',
+               'name': 'incidence_angle'}
+        self.create_band(src, dst)
+        self.dataset.FlushCache()
+
+    def get_full_size_GCPs(self):
+        # Get GCP variables
+        pixel = self.ds['GCP_pixel_' + self.ds.polarisation[:2]][:].data
+        line = self.ds['GCP_line_' + self.ds.polarisation[:2]][:].data
+        lon = self.ds['GCP_longitude_' + self.ds.polarisation[:2]][:].data
+        lat = self.ds['GCP_latitude_' + self.ds.polarisation[:2]][:].data
+        lon = lon.reshape(np.unique(line[:].data).shape[0],
+                np.unique(pixel[:].data).shape[0])
+        lat = lat.reshape(np.unique(line[:].data).shape[0],
+                np.unique(pixel[:].data).shape[0])
+        return lon, lat
+
+    def add_look_direction_band(self):
+        lon, lat = self.get_full_size_GCPs()
+        """
+        TODO: Also in mapper_sentinel1_l1.py... Use this code there..
+        """
+        sat_heading = initial_bearing(lon[:-1, :], lat[:-1, :], lon[1:, :], lat[1:, :])
+        look_direction = scipy.ndimage.interpolation.zoom( np.mod(sat_heading + 90, 360),
+                (np.shape(lon)[0] / (np.shape(lon)[0]-1.), 1))
+
+        # Decompose, to avoid interpolation errors around 0 <-> 360
+        look_direction_u = np.sin(np.deg2rad(look_direction))
+        look_direction_v = np.cos(np.deg2rad(look_direction))
+        look_u_VRT = VRT.from_array(look_direction_u)
+        look_v_VRT = VRT.from_array(look_direction_v)
+        lookVRT = VRT.from_lonlat(lon, lat)
+        lookVRT.create_band([{'SourceFilename': look_u_VRT.filename,
+                               'SourceBand': 1},
+                              {'SourceFilename': look_v_VRT.filename,
+                               'SourceBand': 1}],
+                             {'PixelFunctionType': 'UVToDirectionTo'}
+                             )
+
+        # Blow up to full size
+        lookVRT = lookVRT.get_resized_vrt(self.dataset.RasterXSize, self.dataset.RasterYSize, 1)
+
+        # Store VRTs so that they are accessible later
+        self.band_vrts['look_u_VRT'] = look_u_VRT
+        self.band_vrts['look_v_VRT'] = look_v_VRT
+        self.band_vrts['lookVRT'] = lookVRT
+
+        src = {
+                'SourceFilename': self.band_vrts['lookVRT'].filename,
+                'SourceBand': 1
+            }
+        dst = {
+                'wkv': 'sensor_azimuth_angle',
+                'name': 'look_direction'
+            }
+        self.create_band(src, dst)
+        self.dataset.FlushCache()
+        """ End repetition """
