710b88bb7b56ae728d0069c75ecb582338e7c478
Artem Moiseev <korvinos@bk.ru>
2020-01-17 16:16:02 +0100

    #442 create new tools module

---
+++ nansat/tools.py
@@ -0,0 +1,15 @@
+# Name:    tools.py
+# Purpose: Collection of methods which use core Nansat classes for 
+#          scientific data analysis and visualization 
+# Authors:      Artem Moiseev
+# Created:      17.01.2020
+# Copyright:    (c) NERSC 2020
+# Licence:
+# This file is part of NANSAT.
+# NANSAT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# http://www.gnu.org/licenses/gpl-3.0.html
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
