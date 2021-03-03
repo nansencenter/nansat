c9d427b2ece82970f8d3267a621b3c778ca645ca
aanersc <arash.azamifard@nersc.no>
2021-03-03 07:35:47 +0100

    #464 logging configurations in __init__

--- nansat/__init__.py
+++ nansat/__init__.py
@@ -15,10 +15,14 @@
 # but WITHOUT ANY WARRANTY without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 from __future__ import absolute_import
+import logging.config
 import os
 import sys
+import os.path
 import warnings
 import importlib
+import yaml
+
 pixfun_module_name = 'nansat._pixfun_py{0}'.format(sys.version_info[0])
 
 # check if pixel functions were compiled using setup_tools
@@ -40,4 +44,19 @@ from nansat.figure import Figure
 
 __all__ = ['NSR', 'Domain', 'Nansat', 'Figure']
 
-os.environ['LOG_LEVEL'] = '30'
+#os.environ['LOG_LEVEL'] = '30'
+
+#down below is logging configurations
+DEFAULT_LOGGING_CONF_FILE = os.path.join(os.path.dirname(__file__), 'logging.yml')
+LOGGING_CONF_FILE = os.getenv('NANSAT_LOG_CONF_PATH', DEFAULT_LOGGING_CONF_FILE)
+
+try:
+    with open(LOGGING_CONF_FILE, 'rb') as stream:
+        logging_configuration = yaml.safe_load(stream)  # pylint: disable=invalid-name
+except FileNotFoundError:
+    print(f"'{LOGGING_CONF_FILE}' does not exist, logging can't be configured.", file=sys.stderr)
+    logging_configuration = None  # pylint: disable=invalid-name
+
+if logging_configuration:
+    logging.config.dictConfig(logging_configuration)
+    logging.captureWarnings(True)
